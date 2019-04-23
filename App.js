1e813e73f9dacbe038e89c233b1e47b76dc37e6f
KDavies2020 <kdavies311@gmail.com>
2019-04-23 12:11:29 -0700

    connected react-native with expo. Played around with some native components. Will soon need to think about architecture and layout of files.

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+export default class App extends React.Component {
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <Button
+  onPress={() => {
+    Alert.alert('You tapped the button!');
+  }}
+  title="Press Me"
+/>
+        <Text >Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
