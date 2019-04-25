771a2cf640166b5e130584a85fdb5c7ecc590995
KDavies2020 <kdavies311@gmail.com>
2019-04-25 11:11:04 -0700

    Minimal styling added for boxes on home page. Will now need to adjust text location and then move on to making footer navigation bar

--- App.js
+++ App.js
@@ -24,22 +24,30 @@ export default class App extends React.Component {
     large
 />
 
-       
+       <View style={styles.topBox}>
       <Button color='#bbb'
   onPress={() => {
     Alert.alert('You tapped the button!');
   }}
   title="Start Your Subscription"
 />
+</View>
+<View style={styles.boxes}>
 <Button 
   onPress={() => {
     Alert.alert('You tapped the button!');
   }}
   title="Join Challenge"
 />
+</View>
+<View style={styles.topBox}>
+      <Text>Training Sock Level</Text>
+      <Text>Training Streak</Text>
+</View>
+<View style={styles.topBox}>
+      <Text>Total Time Trained</Text>
+</View>
 
-        <Text >Open up App.js to start working on your app!</Text>
-        <TextInput style={styles.input}></TextInput>
       </View>
     );
   }
@@ -62,8 +70,26 @@ const styles = StyleSheet.create({
     },
     header: {
       marginTop: -400,
+      height: 60,
       backgroundColor: "green"
 
+    },
+    //are these two identical? If so - remove one.
+    topBox: {
+      height: 50,
+      width: 350,
+      borderBottomWidth: 2,
+      borderTopColor: '#bbb',
+      borderBottomColor: '#bbb',
+      marginTop:5,
+    },
+    boxes: {
+      height: 50,
+      width: 350,
+      borderBottomWidth: 2,
+      borderTopColor: '#bbb',
+      borderBottomColor: '#bbb',
+      marginTop:5,
     }
     
 });
